d497950fc1d135b97e4b6c68ba32500aa343e762
Stephen Fenton <stephenefenton@gmail.com>
2018-03-12 23:49:12 -0700

    Finishing Chapter 2
    Finishing Chapter 2- and implementing all bonus exercises as well!

--- app/src/main/java/com/sfenton/geoquiz/QuizActivity.java
+++ app/src/main/java/com/sfenton/geoquiz/QuizActivity.java
@@ -4,28 +4,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
-    Button mButtonTrue;
-    Button mButtonFalse;
+    private Button mButtonFalse;
+    private Button mButtonTrue;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = new Question[] {
+      new Question(R.string.question_awake, false),
+      new Question(R.string.question_bep, true),
+            new Question(R.string.question_ice, true),
+    };
+
+    private int mCurrentIndex = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+
         mButtonTrue = findViewById(R.id.true_button);
-        mButtonFalse = findViewById((R.id.false_button));
+        mButtonFalse = findViewById(R.id.false_button);
+        mNextButton = findViewById(R.id.next_button);
+        mPrevButton = findViewById(R.id.prev_button);
 
         mButtonTrue.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                Toast.makeText(QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
 
@@ -34,10 +52,65 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+
+        mNextButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCurrentIndex == 0)
+                {
+                    mCurrentIndex = mQuestionBank.length - 1;
+                }
+                else
+                {
+                    mCurrentIndex--;
+                }
+                updateQuestion();
+            }
+        });
+
+        mQuestionTextView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+    }
+
+    private void updateQuestion()
+    {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean answer)
+    {
+        if (answer == mQuestionBank[mCurrentIndex].isAnswerTrue())
+        {
+            Toast.makeText(QuizActivity.this,
+                    R.string.correct_toast,
+                    Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(QuizActivity.this,
+                    R.string.incorrect_toast,
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
