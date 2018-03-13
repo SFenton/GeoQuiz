8fbd35a3680f6a970c5bddd5a5d7437b3914e80e
Stephen Fenton <stephenefenton@gmail.com>
2018-03-13 00:20:45 -0700

    Finishing Chapter 3 Implementation.
    Checking in Chapter 3 implementation with the bonus problems done.  However, since we aren't serializing the question bank with answer status, every time we rotate, we aren't saving the state.  So the quiz needs to be done in one rotation pattern, which is kind of sucky, but I don't think I really care right now.

--- app/src/main/java/com/sfenton/geoquiz/Question.java
+++ app/src/main/java/com/sfenton/geoquiz/Question.java
@@ -25,9 +25,30 @@ public class Question {
 
     private boolean mAnswerTrue;
 
+    public boolean getHasBeenAnswered() {
+        return mHasBeenAnswered;
+    }
+
+    public void setHasBeenAnswered(boolean hasBeenAnswered) {
+        mHasBeenAnswered = hasBeenAnswered;
+    }
+
+    private boolean mHasBeenAnswered;
+
+    public boolean isAnsweredCorrectly() {
+        return mAnsweredCorrectly;
+    }
+
+    public void setAnsweredCorrectly(boolean answeredCorrectly) {
+        mAnsweredCorrectly = answeredCorrectly;
+    }
+
+    private boolean mAnsweredCorrectly;
+
     public Question(int textResId, boolean answerTrue)
     {
         mTextResId = textResId;
         mAnswerTrue = answerTrue;
+        mHasBeenAnswered = false;
     }
 }
--- app/src/main/java/com/sfenton/geoquiz/QuizActivity.java
+++ app/src/main/java/com/sfenton/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.sfenton.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -15,6 +16,10 @@ public class QuizActivity extends AppCompatActivity {
     private ImageButton mNextButton;
     private ImageButton mPrevButton;
     private TextView mQuestionTextView;
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+    private static final String BUTTONS_ENABLED = "buttons_enabled";
 
     private Question[] mQuestionBank = new Question[] {
       new Question(R.string.question_awake, false),
@@ -35,6 +40,13 @@ public class QuizActivity extends AppCompatActivity {
 
         mButtonTrue = findViewById(R.id.true_button);
         mButtonFalse = findViewById(R.id.false_button);
+
+        if (savedInstanceState != null)
+        {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            mButtonTrue.setEnabled(savedInstanceState.getBoolean(BUTTONS_ENABLED, true));
+            mButtonFalse.setEnabled(savedInstanceState.getBoolean(BUTTONS_ENABLED, true));
+        }
         mNextButton = findViewById(R.id.next_button);
         mPrevButton = findViewById(R.id.prev_button);
 
@@ -96,6 +108,9 @@ public class QuizActivity extends AppCompatActivity {
     {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
+
+        mButtonFalse.setEnabled(!mQuestionBank[mCurrentIndex].getHasBeenAnswered());
+        mButtonTrue.setEnabled(!mQuestionBank[mCurrentIndex].getHasBeenAnswered());
     }
 
     private void checkAnswer(boolean answer)
@@ -105,12 +120,50 @@ public class QuizActivity extends AppCompatActivity {
             Toast.makeText(QuizActivity.this,
                     R.string.correct_toast,
                     Toast.LENGTH_SHORT).show();
+            mQuestionBank[mCurrentIndex].setAnsweredCorrectly(true);
+            mQuestionBank[mCurrentIndex].setHasBeenAnswered(true);
         }
         else
         {
             Toast.makeText(QuizActivity.this,
                     R.string.incorrect_toast,
                     Toast.LENGTH_SHORT).show();
+
+            mQuestionBank[mCurrentIndex].setAnsweredCorrectly(false);
+            mQuestionBank[mCurrentIndex].setHasBeenAnswered(true);
         }
+
+        int numRight = 0;
+        for (int i = 0; i < mQuestionBank.length; i++)
+        {
+            if (!mQuestionBank[i].getHasBeenAnswered())
+            {
+                break;
+            }
+
+            if (mQuestionBank[i].isAnsweredCorrectly())
+            {
+                numRight++;
+            }
+
+            if (i == mQuestionBank.length - 1)
+            {
+                Toast.makeText(QuizActivity.this,
+                        "Quiz Complete!  Percentage: " + (float)((float)numRight / (float)mQuestionBank.length) * 100.0f + "%",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+
+        mButtonTrue.setEnabled(false);
+        mButtonFalse.setEnabled(false);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle saveInstanceState)
+    {
+        super.onSaveInstanceState(saveInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        saveInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+        saveInstanceState.putBoolean(BUTTONS_ENABLED, mButtonTrue.isEnabled());
     }
 }
